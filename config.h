1950bdd8b61f2a8f4ddb51f32b9183f4a3059864
Micha Niskin <micha.niskin@gmail.com>
2014-12-23 12:30:20 -0500

    working

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char font[] = "Liberation Mono:pixelsize=18:antialias=false:autohint=false";
+static char font[] = "Liberation Mono:pixelsize=34:antialias=false:autohint=false";
 static int borderpx = 2;
 static char shell[] = "/bin/bash";
 
